6d07e847a296aac9e031d645d829469363cf3825
Ben Evans <ben.d.evans@gmail.com>
2017-08-01 22:38:41 +0100

    Refactor controls and rearrange layout

--- brian2gui/run.py
+++ brian2gui/run.py
@@ -25,25 +25,37 @@ class RunInterface(Interface):  # brian2gui.
         super().__init__(*args, **kwargs)
         self.gui = gui  # Top level container
 
+        # TODO: Consolidate the ITEMS/CONTROLS
         self._FIELDS = ['timestep', 'duration']
         self._ITEMS = OrderedDict([('timestep', ipw.Text(description='Timestep, $dt$', value='0.1*ms')),
                                    ('duration', ipw.Text(description='Duration', value='100*ms'))])
         for field in self._FIELDS:
             setattr(self, '_{}'.format(field), self._ITEMS[field])
         #'fa-flask'
-        self._progress = ipw.FloatProgress(description='Progress', icon='fa-hourglass', min=0, max=1)
-        self.children = (ipw.Button(description='Build', button_style='success',
-                                    tooltip='Build', icon='fa-gears'),
-                         self._timestep,
-                         self._duration,
-                         ipw.Button(description='Run', button_style='success',
-                                    tooltip='Run', icon='fa-play'),
-                         self._progress,
-                         ipw.Button(description='Save', button_style='info',
-                                    tooltip='Save', icon='fa-save'),
-                         ipw.Button(description='Load', button_style='info',
-                                    tooltip='Load', icon='fa-sign-in'),
-                         ipw.Text(description='Filename'))
+        controls = [('Build', ipw.Button(description='Build', tooltip='Build',
+                                         button_style='success', icon='fa-gears')),
+                    ('Run', ipw.Button(description='Run', tooltip='Run',
+                                       button_style='success', icon='fa-play')),
+                    ('Progress', ipw.FloatProgress(description='Progress',
+                                                   min=0, max=1,
+                                                   icon='fa-hourglass')),
+                    ('Save', ipw.Button(description='Save', tooltip='Save',
+                                        button_style='info', icon='fa-save')),
+
+                    ('Load', ipw.Button(description='Load', tooltip='Load',
+                                        button_style='info', icon='fa-sign-in')),
+                    ('Filename', ipw.Text(description='Filename'))]
+        self._CONTROLS = OrderedDict(controls)
+
+        self.children = (ipw.HBox(children=[self._CONTROLS['Filename'],
+                                            self._CONTROLS['Save'],
+                                            self._CONTROLS['Load']]),
+                         ipw.HBox(children=[self._CONTROLS['Build'],
+                                            self._timestep,
+                                            self._duration,
+                                            self._CONTROLS['Run'],
+                                            self._CONTROLS['Progress']]))
+
                          #(ipw.HBox(children=list(self._CONTROLS.values())),
                          #ipw.HBox(children=self._LABELS),
                          #self.ENTRY_BOX)
