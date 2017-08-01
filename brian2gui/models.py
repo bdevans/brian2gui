8b9651d9c80df5001097efc971f13b6a38a081e3
Ben Evans <ben.d.evans@gmail.com>
2017-08-01 22:04:42 +0100

    Change second to ms and tweak eqs

--- brian2gui/models.py
+++ brian2gui/models.py
@@ -18,7 +18,7 @@ b : 1/second
 c : volt
 d : volt/second
 amplitude : volt
-tau : msecond'''
+tau : ms'''
 
 # Introduced in Brette R. and Gerstner W. (2005), Adaptive Exponential
 # Integrate-and-Fire Model as an Effective Description of Neuronal Activity,
@@ -30,12 +30,12 @@ I : amp'''
 
 # https://brian2.readthedocs.io/en/2.0rc3/examples/IF_curve_Hodgkin_Huxley.html
 HH = '''dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
-dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT) /
     (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
     (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
-dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT) /
     (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
-dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV)) / ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
 I : amp'''
 
 NEURON_MODELS = OD([('Blank', OD([('model', ''), ('threshold', ''), ('reset', ''), ('refractory', '')])),
